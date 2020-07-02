5d145f0807ae628ab74de3412e9cd6f7d920767e
ryanholly3 <ryanholly3@gmail.com>
2020-07-02 14:45:44 -0600

    Add detail to About Me

--- src/components/AboutMe/index.js
+++ src/components/AboutMe/index.js
@@ -1,16 +1,27 @@
 import React, { Component } from 'react';
-import { Grid, Card, CardContent, Avatar } from '@material-ui/core';
+import { Grid, Card, CardContent, Avatar,Typography } from '@material-ui/core';
 import './styles.css';
 
 export default class AboutMe extends Component {
   render() {
     return (
       <>
-        <Grid container spacing={6}>
-          <Grid item xs={12} sm={6}>
+        <Grid container spacing={6} style={{ height: '100%', display: 'flex', alignItems:'center'}}>
+          <Grid item xs={12} sm={5}>
             <Card>
               <CardContent>
-                <Avatar variant='rounded' alt="Ryan Holly" src="/ryan.jpeg" style={{ width: '400px', height: "400px"}} />
+                <Typography variant='h3'>
+                  About Me
+                </Typography>
+                <Card style={{ 
+                  height: '500px',
+                  width: '100%',
+                  textAlign: 'center',
+                  backgroundImage: 'url("/ryan.jpeg")',
+                  backgroundRepeat: 'no-repeat',
+                  backgroundSize: 'cover',
+                  backgroundPosition: 'center'
+                }}/>
               </CardContent>
             </Card>
           </Grid>
