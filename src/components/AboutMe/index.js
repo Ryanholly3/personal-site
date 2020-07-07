fe1a83faf54073849f8b7546eb6d32ca63e27dce
ryanholly3 <ryanholly3@gmail.com>
2020-07-06 21:39:59 -0600

    Add experience calculation

--- src/components/AboutMe/index.js
+++ src/components/AboutMe/index.js
@@ -1,20 +1,57 @@
 import React, { Component } from 'react';
-import { Grid, Card, CardContent, Avatar,Typography } from '@material-ui/core';
+import { Grid, Card, CardContent, Typography } from '@material-ui/core';
 import './styles.css';
 
 export default class AboutMe extends Component {
+  constructor(props){
+    super(props)
+    this.experience = {
+      years: null,
+      months: null
+    }
+    this.calculateExperience()
+  }
+  
+  calculateExperience = () => {
+    let now = new Date()
+    let startDate = new Date(2018, 11)
+    let totalMonths = this.calculateMonths(startDate, now)
+    let years = Math.floor(totalMonths / 12)
+    let months = totalMonths - (years * 12)
+    this.experience = {
+      years: years,
+      months: months
+    }
+  }
+
+  calculateMonths = (dateFrom, dateTo) => {
+    return dateTo.getMonth() - dateFrom.getMonth() + (12 * (dateTo.getFullYear() - dateFrom.getFullYear()))
+  }
+
+
   render() {
+    const { years, months } = this.experience
+
+   let monthString
+    if(months === 0){
+      monthString = ''
+    } else if (months === 1){
+      monthString = '1 month'
+    } else {
+      monthString = `${months} months`
+    }
+
     return (
       <>
         <Grid container spacing={6} style={{ height: '100%', display: 'flex', alignItems:'center'}}>
-          <Grid item xs={12} sm={5}>
+          <Grid item xs={12} sm={4}>
             <Card>
               <CardContent>
                 <Typography variant='h3'>
                   About Me
                 </Typography>
                 <Card style={{ 
-                  height: '500px',
+                  height: '400px',
                   width: '100%',
                   textAlign: 'center',
                   backgroundImage: 'url("/ryan.jpeg")',
@@ -25,15 +62,14 @@ export default class AboutMe extends Component {
               </CardContent>
             </Card>
           </Grid>
-          <Grid item xs={12} sm={6}>
+          <Grid item xs={12} sm={8}>
             <Card>
               <CardContent>
-                Software Developer at Trelora Real Estate. 
-                My biology research background gives me a 
-                scientific approach to solving problems and 
-                writing code. I love to solve complex 
-                problems creatively, to learn, and to work 
-                on motivated teams
+                Currently a Full-stack developer at Trelora Real Estate.
+
+                With {years} year{years > 1 ? 's' : ''} {monthString} of professional software experience,
+                
+
               </CardContent>
             </Card>
           </Grid>
